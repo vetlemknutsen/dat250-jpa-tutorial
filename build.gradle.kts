a3c00eef51d67238e3b2c61f7c4cdfab42aeaabd
Vetle Knutsen <vetleknutsen1@gmail.com>
2024-10-08 13:02:54 +0200

    did changes for expass7

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.hibernate.orm:hibernate-core")
     implementation("jakarta.transaction:jakarta.transaction-api")
     implementation("com.h2database:h2:2.2.220")
+    implementation("org.postgresql:postgresql:42.2.23")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
